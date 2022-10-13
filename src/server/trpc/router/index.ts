4976f7d75d0678c7905d1a1a5ae94ec6cc139478
befigenio <befigenio@gmail.com>
2022-10-13 19:18:16 +0300

    add sample router to forms

--- src/server/trpc/router/index.ts
+++ src/server/trpc/router/index.ts
@@ -2,9 +2,11 @@
 import { t } from "../trpc";
 
 import { exampleRouter } from "./example";
+import { formsRouter } from "./forms";
 
 export const appRouter = t.router({
   example: exampleRouter,
+  forms: formsRouter,
 });
 
 // export type definition of API
