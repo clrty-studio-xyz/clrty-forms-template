0801c63d0e9c207b675529387739af4c6441a721
befigenio <befigenio@gmail.com>
2022-10-13 11:21:42 +0300

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+// src/pages/_app.tsx
+import "../styles/globals.css";
+import type { AppType } from "next/app";
+import { trpc } from "../utils/trpc";
+import { SessionProvider } from "next-auth/react";
+
+const MyApp: AppType = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+export default trpc.withTRPC(MyApp);
