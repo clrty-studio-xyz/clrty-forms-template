79c1a945a4248a64cf6bbcdf0a480aafbecdf110
befigenio <befigenio@gmail.com>
2022-10-13 15:23:41 +0300

    layout for drag and drop form builder

--- src/pages/form/[form].tsx
+++ src/pages/form/[form].tsx
@@ -1,9 +1,20 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { trpc } from "../../utils/trpc";
+import { DndContext } from "@dnd-kit/core";
+import { Draggable } from "../../components/Form/Dragabble";
+import { useState } from "react";
+import { Droppable } from "../../components/Form/Droppable";
 
 const Form: NextPage = () => {
+  const [isDropped, setIsDropped] = useState(false);
   const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
+
+  const handleDragEnd = (event) => {
+    if (event.over && event.over.id === "droppable") {
+      setIsDropped(true);
+    }
+  };
 
   return (
     <>
@@ -13,22 +24,17 @@ const Form: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="relative z-0 flex flex-1 overflow-hidden">
-        <main className="relative z-0 flex-1 overflow-y-auto focus:outline-none xl:order-last">
-          <div className="absolute inset-0 py-6 px-4 sm:px-6 lg:px-8">
-            <div className="h-full rounded-lg border-2 border-dashed border-gray-200">
-              left
-            </div>
+      <div className="flex">
+        <DndContext onDragEnd={handleDragEnd}>
+          <div className="w-96 flex-none">
+            {!isDropped ? <Draggable>Drag Me</Draggable> : null}
           </div>
-          {/* End main area */}
-        </main>
-        <aside className="relative hidden w-96 flex-shrink-0 overflow-y-auto border-r border-gray-200 xl:order-first xl:flex xl:flex-col">
-          <div className="absolute inset-0 py-6 px-4 sm:px-6 lg:px-8">
-            <div className="h-full rounded-lg border-2 border-dashed border-gray-200">
-              main
-            </div>
+          <div className="flex-1">
+            <Droppable className="h-full rounded-lg border-2 border-dashed border-gray-200">
+              {isDropped ? <Draggable>Drag Me</Draggable> : "Drop Here"}
+            </Droppable>
           </div>
-        </aside>
+        </DndContext>
       </div>
     </>
   );
